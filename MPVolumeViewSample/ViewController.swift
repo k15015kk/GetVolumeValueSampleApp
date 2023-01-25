4f42892a5d66bf6a632a168a4e73621fbda1d5aa
inoueharuki <inoue.haruki@multisoup.co.jp>
2023-01-25 17:36:39 +0900

    add: デバイスの音量を表示する機能を実装

--- MPVolumeViewSample/ViewController.swift
+++ MPVolumeViewSample/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlet
+    @IBOutlet private weak var getVolumeButton: UIButton!
+    @IBOutlet private weak var displayVolumeLabel: UILabel!
+    
+    // MARK: - OtherView
+    private let volumeView: UIView = MPVolumeView(frame: CGRect.zero)
+    
+    // MARK: - Properties
+    private var volume: Float = 0.0 {
+        didSet {
+            displayVolumeLabel.text = "Volume: \(volume)"
+        }
+    }
+    
+    // MARK: - LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    // MARK: - IBAction
+    @IBAction func getVolumeAction(_ sender: Any) {
+        volume = self.getVolumeValue()
+    }
+    
+}
 
-
+extension ViewController {
+    
+    /// デバイスのボリューム値を取得します
+    /// - Returns: デバイスのボリューム値
+    func getVolumeValue() -> Float {
+        
+        var volume: Float = -1.0
+        
+        /// ボリュームスライダーの取得
+        /// スライダーの取得ができなかった場合は -1.0 をそのまま返す
+        guard let slider = volumeView.subviews.compactMap({$0 as? UISlider}).first else {
+            return volume
+        }
+        
+        // スライダーの値を格納して、値を関数から返す
+        volume = slider.value
+        return volume
+    }
 }
 

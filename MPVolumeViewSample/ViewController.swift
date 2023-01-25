07bb40835cb05e034da170a26e8a125c64877a04
inoueharuki <inoue.haruki@multisoup.co.jp>
2023-01-25 14:40:58 +0900

    Initial Commit

---
+++ MPVolumeViewSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MPVolumeViewSample
+//
+//  Created by Inoue Haruki on 2023/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
